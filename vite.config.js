2f3ee8d90d657adf94da909660e1eff16dd508e7
Danymag <daniil.magula@gmail.com>
2025-01-17 00:53:01 +0300

    2build

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          if (/\.png|\.jpg|\.jpeg|\.svg|\.gif|\.webp$/.test(assetInfo.name)) {
+            return 'assets/images/[name][extname]';
+          }
+          return 'assets/[name][extname]';
+        },
+      },
+    },
+  },
 })
+
+
+
